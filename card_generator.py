17ba880b8d950a9ff5e5f38eb690d9243e1a1a50
Paulina Mróz <15126365+paulina-mroz@users.noreply.github.com>
2021-07-09 20:36:46 +0200

    Update card_generator.py
    compact cards on the page; add back of the cards to match cards after printing; add damage cards numbers

--- card_generator.py
+++ card_generator.py
@@ -55,20 +55,69 @@ class CardTexGenerator(object):
         self.print_debug("listCardsToProcess done")
 
     def processCards (self):
+        currRow = 0
+        currCol = 0
+        limitRow = 4
+        limitCol = 4
+        texAll = ""
+        texAllTmp = ""
+        texPage = ""
+        texPageTmp = ""
+        texBackPage = ""
+        texBackPageTmp = ""
+        texRow = ""
+        texBackRow = ""
         for card in self.cardToProcess:
-            self.cardTex(card)
+            texCard = self.cardTex(card)
+            texCardBack = self.cardBackTex(card)
+
+            texRow = texRow + texCard
+            texBackRow = texCardBack + texBackRow
+            texPageTmp = texPage + texRow + "\n\\vspace{1ex}\n"
+            texBackPageTmp = texBackPage + texBackRow + "\n\\vspace{1ex}\n"
+            texAllTmp = texAll + texPageTmp + "\n\\newpage\n"
+            texAllTmp = texAllTmp + texBackPageTmp + "\n\\newpage\n"
+            currCol = currCol + 1
+            if currCol >= limitCol:
+                currCol = 0
+                texRow = ""
+                texBackRow = ""
+                texPage = texPageTmp
+                texBackPage = texBackPageTmp
+                currRow = currRow + 1
+                if currRow >= limitRow:
+                    currRow = 0
+                    texPage = ""
+                    texBackPage = ""
+                    texAll = texAllTmp
+        texAll = texAllTmp
+        self.addTex(texAll)
+
         self.print_debug("processCards done")
 
     def cardTex (self, card):
+        tex = ""
         type = card["type"]
         if type == "damage":
-            self.cardDamageTex(card)
-            self.cardBackDamageTex(card)
+            tex = self.cardDamageTex(card)
         # elif x == 'b':
         else:
             print("Card not found!")
             # print(card)
+        return tex
         self.print_debug("cardTex done")
+
+    def cardBackTex (self, card):
+        tex = ""
+        type = card["type"]
+        if type == "damage":
+            tex = self.cardBackDamageTex(card)
+        # elif x == 'b':
+        else:
+            print("Card not found!")
+            # print(card)
+        return tex
+        self.print_debug("cardBackTex done")
 
     def tikzCommand (self, style):
         if style == "clip":
@@ -115,29 +164,32 @@ class CardTexGenerator(object):
         return command
 
     def cardDamageTex (self, card):
+        tex = ""
         size = self.cardParams["sizes"][card["type"]]["mini"]
 
         line_width = size["line_width"]
         cw = size["card_width"]
         ch = size["card_height"]
 
-        self.addTex(r"\begin{tikzpicture}")
-        self.addTex(r"[")
-        self.addTex(r"border_style/.style      = {{ fill=border_fill  , draw=none         , line width={:.2f}pt , rounded corners=0.0cm    }},".format(0.0))
-        self.addTex(r"card_style/.style        = {{ fill=card_fill    , draw=card_line    , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["card_round"]))
-        self.addTex(r"textbox_style/.style     = {{ fill=textbox_fill , draw=textbox_line , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["textbox_round"]))
-        self.addTex(r"panel_lines_style/.style = {{ fill=card_fill    , draw=panel_line   , line width={:.2f}pt , rounded corners=0.0cm    }},".format(line_width))
-        self.addTex(r"panel_fill_style/.style = {{ fill=panel_fill , draw=none       , line width={:.2f}pt }},".format(0.0))
-        self.addTex(r"panel_line_style/.style = {{ fill=none       , draw=panel_line , line width={:.2f}pt }},".format(line_width))
-        self.addTex(r"title_style/.style  = { rectangle , inner sep=0.05cm, minimum height = 1cm, fill=title_fill , draw=none , text=title_font , line width=0.0pt , font=\scshape\bfseries },")
-        self.addTex(r"text_style/.style  =  { rectangle , inner sep=0.05cm, below , align=center , fill=none , draw=none , text=black , font=\scriptsize }")
-        self.addTex(r"]")
+        tex = tex + r"\begin{tikzpicture}" + "\n"
+        tex = tex + r"[" + "\n"
+        tex = tex + r"border_style/.style      = {{ fill=border_fill  , draw=none         , line width={:.2f}pt , rounded corners=0.0cm    }},".format(0.0) + "\n"
+        tex = tex + r"card_style/.style        = {{ fill=card_fill    , draw=card_line    , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["card_round"]) + "\n"
+        tex = tex + r"textbox_style/.style     = {{ fill=textbox_fill , draw=textbox_line , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["textbox_round"]) + "\n"
+        tex = tex + r"panel_lines_style/.style = {{ fill=card_fill    , draw=panel_line   , line width={:.2f}pt , rounded corners=0.0cm    }},".format(line_width) + "\n"
+        tex = tex + r"panel_fill_style/.style = {{ fill=panel_fill , draw=none       , line width={:.2f}pt }},".format(0.0) + "\n"
+        tex = tex + r"panel_line_style/.style = {{ fill=none       , draw=panel_line , line width={:.2f}pt }},".format(line_width) + "\n"
+        tex = tex + r"title_style/.style  = { rectangle , inner sep=0.05cm, minimum height = 1cm, fill=title_fill , draw=none , text=title_font , line width=0.0pt , font=\scshape\bfseries }," + "\n"
+        tex = tex + r"text_style/.style  =  { rectangle , inner sep=0.05cm , align=center , fill=none , draw=none , text=black , font=\scriptsize }," + "\n"
+        tex = tex + r"id_number_style/.style  =  { rectangle , inner sep=0.05cm , align=center , fill=none , draw=none , text=panel_line , font=\tiny }" + "\n"
+        # tex = tex + r"text_style/.style  =  { rectangle , inner sep=0.05cm, below , align=center , fill=none , draw=none , text=black , font=\scriptsize }" + "\n"
+        tex = tex + r"]" + "\n"
 
         border_thick = size["border_thick"]
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzRectangle("border_style", 0-border_thick, 0-border_thick, cw+border_thick, ch+border_thick))
-        self.addTex(self.tikzRectangle("card_style", 0, 0, cw, ch))
-        self.addTex(r"\end{scope}")
+        tex = tex + r"\begin{scope}" + "\n"
+        # tex = tex + self.tikzRectangle("border_style", 0-border_thick, 0-border_thick, cw+border_thick, ch+border_thick) + "\n"
+        tex = tex + self.tikzRectangle("card_style", 0, 0, cw, ch) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
         title_height = size["title_height"]
         panel_thick  = size["panel_thick"]
@@ -146,36 +198,46 @@ class CardTexGenerator(object):
         panel_outer_x2 = cw - panel_gap
         panel_outer_y1 = 0 + panel_gap
         panel_outer_y2 = ch - panel_gap
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzCShape("clip"            , panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick))
-        self.addTex(self.tikzCShape("panel_fill_style", panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick))
+        tex = tex + r"\begin{scope}" + "\n"
+        tex = tex + self.tikzCShape("clip"            , panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick) + "\n"
+        tex = tex + self.tikzCShape("panel_fill_style", panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick) + "\n"
         panel_line1_x1 = panel_outer_x1+1.2*panel_thick
         panel_line2_y2 = panel_outer_y2-1.2*title_height
+        panel_line3_x1 = panel_outer_x2-1.2*panel_thick
         panel_line_thick = 0.05
-        self.addTex(self.tikzRectangle("panel_lines_style", panel_line1_x1, 0, panel_line1_x1+panel_line_thick, ch))
-        self.addTex(self.tikzRectangle("panel_lines_style", 0, panel_line2_y2-panel_line_thick, cw, panel_line2_y2))
-        self.addTex(self.tikzResizedTextNode("title_style", cw/2 + 0.05, panel_outer_y2 - title_height/2, 0.35, 1.0, title_height - 0.1, card["name"].upper()))
-        self.addTex(r"\end{scope}")
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzCShape("panel_line_style", panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick))
-        self.addTex(r"\end{scope}")
+        tex = tex + self.tikzRectangle("panel_lines_style", panel_line1_x1, 0, panel_line1_x1+panel_line_thick, ch) + "\n"
+        tex = tex + self.tikzRectangle("panel_lines_style", 0, panel_line2_y2-panel_line_thick, cw, panel_line2_y2) + "\n"
+        tex = tex + self.tikzRectangle("panel_lines_style", panel_line3_x1-panel_line_thick, 0, panel_line3_x1, ch/2) + "\n"
+        tex = tex + self.tikzResizedTextNode("title_style", cw/2 + 0.05, panel_outer_y2 - title_height/2, 0.35, 1.0, title_height - 0.1, card["name"].upper()) + "\n"
+        tex = tex + self.tikzTextNode("id_number_style", panel_line3_x1+((panel_outer_x2-panel_line3_x1)/2), panel_outer_y1+panel_thick/2, card["id"]) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
+        tex = tex + r"\begin{scope}" + "\n"
+        tex = tex + self.tikzCShape("panel_line_style", panel_outer_x1, panel_outer_y1, panel_outer_x2, panel_outer_y2, title_height, panel_thick) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
         textbox_gap = size["textbox_gap"]
         textbox_x1 = panel_outer_x1 + panel_thick + textbox_gap
         textbox_y1 = panel_outer_y1 + panel_thick + textbox_gap
         textbox_x2 = cw - panel_gap - textbox_gap
         textbox_y2 = panel_outer_y2 - title_height - textbox_gap
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzRectangle("textbox_style", textbox_x1, textbox_y1, textbox_x2, textbox_y2))
+        tex = tex + r"\begin{scope}" + "\n"
+        tex = tex + self.tikzRectangle("textbox_style", textbox_x1, textbox_y1, textbox_x2, textbox_y2) + "\n"
 
         card_text_from_json = card["text"]
         card_text_tmp = card_text_from_json.replace("\n\n", "\n\n\\vspace{1em}\n")
+        card_text_tmp = card_text_tmp.replace("[hit]", "\inlinegraphics{../pics_vector/hit.pdf}")
+        card_text_tmp = card_text_tmp.replace("[crit]", "\inlinegraphics{../pics_vector/crit.pdf}")
+        card_text_tmp = card_text_tmp.replace("[talent]", "\inlinegraphics{../pics_vector/talent.pdf}")
+        card_text_tmp = card_text_tmp.replace("[turn-left]", "\inlinegraphics{../pics_vector/turn-left.pdf}")
+        card_text_tmp = card_text_tmp.replace("[turn-right]", "\inlinegraphics{../pics_vector/turn-right.pdf}")
+        card_text_tmp = card_text_tmp.replace("[weapon]", "\inlinegraphics{../pics_vector/weapon.pdf}")
         card_text = card_text_tmp.replace("ACTION:", "\\textbf{ACTION:}")
-        self.addTex(self.tikzTextNode("text_style, text width={:.2f}cm".format(textbox_x2-textbox_x1-0.1), textbox_x1+((textbox_x2-textbox_x1)/2), textbox_y2-size["textbox_round"], card_text))
-        self.addTex(r"\end{scope}")
+        # tex = tex + self.tikzTextNode("text_style, text width={:.2f}cm".format(textbox_x2-textbox_x1-0.1), textbox_x1+((textbox_x2-textbox_x1)/2), textbox_y2-size["textbox_round"], card_text) + "\n"
+        tex = tex + self.tikzTextNode("text_style, text width={:.2f}cm".format(textbox_x2-textbox_x1-0.2), textbox_x1+((textbox_x2-textbox_x1)/2), textbox_y2-((textbox_y2-textbox_y1)/2), card_text) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
-        self.addTex(r"\end{tikzpicture}")
-        self.addTex(r"")
+        tex = tex + r"\end{tikzpicture}" + "\n"
+        return tex
 
     def tikzDamageBackHit (self, style):
         prefix = self.tikzCommand(style)
@@ -223,27 +285,28 @@ class CardTexGenerator(object):
         return command
 
     def cardBackDamageTex (self, card):
+        tex = ""
         size = self.cardParams["sizes"][card["type"]]["mini"]
 
         line_width = size["line_width"]
         cw = size["card_width"]
         ch = size["card_height"]
 
-        self.addTex(r"\begin{tikzpicture}")
-        self.addTex(r"[")
-        self.addTex(r"border_style/.style  = {{ fill=border_fill  , draw=none         , line width={:.2f}pt    , rounded corners=0.0cm    }},".format(0.0))
-        self.addTex(r"card_style/.style    = {{ fill=dmg_back_card_fill , draw=dmg_back_card_line , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["card_round"]))
-        self.addTex(r"circle_style/.style  = {{ fill=dmg_back_card_fill!60!black , draw=dmg_back_card_line , line width={:.2f}pt }},".format(line_width))
-        self.addTex(r"hitbox_style/.style  = {{ rectangle , inner sep=0.0cm , align=center , fill=none , draw=none, line width={:.2f}pt }}".format(0.0))
-        self.addTex(r"]")
+        tex = tex + r"\begin{tikzpicture}" + "\n"
+        tex = tex + r"[" + "\n"
+        tex = tex + r"border_style/.style  = {{ fill=border_fill  , draw=none         , line width={:.2f}pt    , rounded corners=0.0cm    }},".format(0.0) + "\n"
+        tex = tex + r"card_style/.style    = {{ fill=dmg_back_card_fill , draw=dmg_back_card_line , line width={:.2f}pt , rounded corners={:.2f}cm }},".format(line_width, size["card_round"]) + "\n"
+        tex = tex + r"circle_style/.style  = {{ fill=dmg_back_card_fill!60!black , draw=dmg_back_card_line , line width={:.2f}pt }},".format(line_width) + "\n"
+        tex = tex + r"hitbox_style/.style  = {{ rectangle , inner sep=0.0cm , align=center , fill=none , draw=none, line width={:.2f}pt }}".format(0.0) + "\n"
+        tex = tex + r"]" + "\n"
 
         border_thick = size["border_thick"]
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzRectangle("border_style", 0-border_thick, 0-border_thick, cw+border_thick, ch+border_thick))
-        self.addTex(self.tikzRectangle("card_style", 0, 0, cw, ch))
-        self.addTex(r"\end{scope}")
+        tex = tex + r"\begin{scope}" + "\n"
+        # tex = tex + self.tikzRectangle("border_style", 0-border_thick, 0-border_thick, cw+border_thick, ch+border_thick) + "\n"
+        tex = tex + self.tikzRectangle("card_style", 0, 0, cw, ch) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
-        self.addTex(r"\begin{scope}")
+        tex = tex + r"\begin{scope}" + "\n"
         back_hit = r"\resizebox{{ {:.2f}cm }}{{!}}{{".format(0.6*cw) + "\n"
         back_hit = back_hit + r"\begin{tikzpicture}" + "\n"
         back_hit = back_hit + r"[" + "\n"
@@ -253,26 +316,26 @@ class CardTexGenerator(object):
         back_hit = back_hit + self.tikzDamageBackHit("hit_style") + "\n"
         back_hit = back_hit + r"\end{tikzpicture}"
         back_hit = back_hit + "}" + "\n"
-        self.addTex(self.tikzTextNode("hitbox_style", cw/2, ch/2, back_hit))
-        self.addTex(r"\end{scope}")
+        tex = tex + self.tikzTextNode("hitbox_style", cw/2, ch/2, back_hit) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
         circle_r = 0.1
         circle_gap = 3*circle_r
-        self.addTex(r"\begin{scope}")
-        self.addTex(self.tikzCircle("circle_style", 0+circle_gap, 0+circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", 0+circle_gap, ch/4+0.5*circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", 0+circle_gap, ch/2, circle_r))
-        self.addTex(self.tikzCircle("circle_style", 0+circle_gap, ch*3/4-0.5*circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", 0+circle_gap, ch-circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", cw-circle_gap, 0+circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", cw-circle_gap, ch/4+0.5*circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", cw-circle_gap, ch/2, circle_r))
-        self.addTex(self.tikzCircle("circle_style", cw-circle_gap, ch*3/4-0.5*circle_gap, circle_r))
-        self.addTex(self.tikzCircle("circle_style", cw-circle_gap, ch-circle_gap, circle_r))
-        self.addTex(r"\end{scope}")
+        tex = tex + r"\begin{scope}" + "\n"
+        tex = tex + self.tikzCircle("circle_style", 0+circle_gap, 0+circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", 0+circle_gap, ch/4+0.5*circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", 0+circle_gap, ch/2, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", 0+circle_gap, ch*3/4-0.5*circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", 0+circle_gap, ch-circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", cw-circle_gap, 0+circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", cw-circle_gap, ch/4+0.5*circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", cw-circle_gap, ch/2, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", cw-circle_gap, ch*3/4-0.5*circle_gap, circle_r) + "\n"
+        tex = tex + self.tikzCircle("circle_style", cw-circle_gap, ch-circle_gap, circle_r) + "\n"
+        tex = tex + r"\end{scope}" + "\n"
 
-        self.addTex(r"\end{tikzpicture}")
-        self.addTex(r"")
+        tex = tex + r"\end{tikzpicture}" + "\n"
+        return tex
 
     def writeTexFile (self):
         # print(self.texContent)
